cc6ffa6eb9d64a2a7c0770821ef0554a72a60f85
Harshitcoding <khandelwalharshit96@gmail.com>
2026-05-07 14:08:59 +0530

    chore: update server with all routes and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
-import express from 'express';
 import dotenv from 'dotenv';
+dotenv.config(); // ← Sabse pehle, baki sab ke upar
+
+import express from 'express';
 import dns from 'dns';
 import cors from 'cors';
+
 import connectDB from './src/config/db.js';
 import scrapeHackerNews from './src/scraper/scraper.js';
 
+import scraperRoutes from './src/routes/scraper.routes.js';
+import authRoutes from './src/routes/auth.routes.js';
 
-dns.setServers(["8.8.8.8", "1.1.1.1"]) 
-
-dotenv.config();
-connectDB();
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', scraperRoutes);
+app.use('/api/auth', authRoutes);
+
+connectDB();
 scrapeHackerNews();
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
