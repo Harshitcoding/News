ad72c5b45a1d632b980ad9b4ceba1cdc73630076
Harshitcoding <khandelwalharshit96@gmail.com>
2026-05-07 14:08:29 +0530

    feat: add story mongoose model

--- backend/src/models/story.model.js
+++ backend/src/models/story.model.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
 
 const storySchema = new mongoose.Schema({
-  title: { type: String, required: true, },
-  url: { type: String, unique: true },
+  title: { type: String, required: true, unique: true},
+  url: { type: String },
   points: { type: Number, default: 0 },
   author: { type: String },
   postedAt: { type: String },
